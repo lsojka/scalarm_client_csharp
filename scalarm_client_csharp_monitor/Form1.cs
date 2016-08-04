9f911c32560bef7c6a8c6dc6bdf152a15da5f522
Łukasz Sojka <zoltygawron@gmail.com>
2016-08-04 19:52:08 +0200

    Added "monitor" project
    New "monitor" WinForm application.

---
+++ scalarm_client_csharp_monitor/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Scalarm;
+
+
+namespace scalarm_client_csharp_monitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("220 v");
+            ExperimentStatistics h;
+        }
+    }
+}
